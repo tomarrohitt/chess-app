7fbeb51c521982dd2663f34d9053fd57ef77f97a
Rohit Tomar <tomarrohit5034@gmail.com>
2026-05-15 18:43:38 +0530

    config for remote db connection.

--- chess-monolith/src/seed/setup.ts
+++ chess-monolith/src/seed/setup.ts
@@ -7,9 +7,9 @@ const sql = postgres(env.DATABASE_URL!);
 async function main() {
   try {
     await sql`CREATE EXTENSION IF NOT EXISTS pg_trgm;`;
-    console.log("✅ Extension 'pg_trgm' created or already exists.");
+    console.log("Extension 'pg_trgm' created or already exists.");
   } catch (error) {
-    console.error("❌ Failed to create extension:", error);
+    console.error("Failed to create extension:", error);
   } finally {
     process.exit(0);
   }
