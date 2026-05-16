9fe2c669caac336e3093ac792591b108f66ab894
Rohit Tomar <tomarrohit5034@gmail.com>
2026-05-16 15:06:04 +0530

    Move all files to repository root for Hugging Face

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,16 +2,16 @@ import "dotenv/config";
 import { z, ZodType } from "zod";
 
 const schema = z.object({
-  PORT: z.coerce.number(),
+  PORT: z.coerce.number().default(8080),
   CLIENT_URL: z.string().default("http://localhost:3000"),
   DATABASE_URL: z
     .string()
     .default("postgresql://postgres:password@localhost:5432/chess"),
 
   BETTER_AUTH_URL: z.string().default("http://localhost:7860"),
-  CLOUDINARY_URL: z.string(),
+  CLOUDINARY_URL: z.string().default(""),
   REDIS_URL: z.string().default("redis://localhost:6379"),
-  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_SECRET: z.string().default(""),
 });
 
 export function validateEnv<T>(schema: ZodType<T>): T {
